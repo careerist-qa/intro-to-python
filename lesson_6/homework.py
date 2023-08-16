b34f070754f7ec732430cde8c73b0bacec3c5c8a
Joseph Sabido <joesabido@gmail.com>
2023-08-16 15:51:46 -0500

    Homework example exercise

--- lesson_6/homework.py
+++ lesson_6/homework.py
@@ -1,40 +1,43 @@
-# FUNCTIONS
+# READ CAREFULLY TO THE END BEFORE YOU START SOLVING THE EXERCISES.
+# ----------------------------------------------------------------------------------------------------------------------
 
-# Difference
-# Write a function, which will calculate the difference of these two numbers
+# Homework Lesson 6: Lists
+# Please use concepts and tools you've learned in previous lessons whenever you can.
 
-def difference(num_1, num_2):
-    pass
+# EXAMPLE EXERCISE
+# We solved this one for you, to give you an idea of what we expect you to do.
+
+# You have the following team members at work: Eric, Tom, Kelly and You.
+# Your company wants to do a lottery, in such a way that those born on an EVEN (divisible by 2) number year receive a
+# prize. Create a little program that will ask for the birth years of you and your teammates and builds a list of those
+# that will receive a prize. Print the result at the end.
+
+# Create an empty list.
+prize_receivers = []
+
+# Take the year input and cast it as int so we can perform a modulus calculation on it later.
+birth_year_eric = int(input("Eric's birth year:"))
+birth_year_tom = int(input("Tom's birth year:"))
+birth_year_kelly = int(input("Kelly's birth year:"))
+birth_year_you = int(input("Your birth year:"))
+
+# Figure out if any of the years are multiples of two, by using the modulus operator (%).
+# Those that are, are added to the prize_receivers list.
+if birth_year_eric % 2 == 0:
+    prize_receivers.append("Eric")
+
+if birth_year_tom % 2 == 0:
+    prize_receivers.append("Tom")
+
+if birth_year_kelly % 2 == 0:
+    prize_receivers.append("Kelly")
+
+if birth_year_you % 2 == 0:
+    prize_receivers.append("You")
+
+# Print the result.
+print("The following people receive a prize:")
+print(prize_receivers)
 
 
-# Division
-# Write a function, which will divide these two numbers
 
-def division(num_1, num_2):
-    pass
-
-
-# Function gets random number. If this number is more than ten, return the difference between 100 and this number,
-# otherwise return this number multiplied by 10
-
-def function_1(number):
-    pass
-
-
-# Your function temerature_convertor gets the temperature in Fahrenheit, convert it to Celsius and return.
-# Formula (32°F − 32) × 5/9 = 0°C
-
-def temerature_convertor(fahrenheit_degree):
-    pass
-
-
-# Taxi Fare
-# In a particular jurisdiction, taxi fares consist of a base fare of $4.00, plus $0.25 for every 140 meters travelled.
-# Write a function that takes the distance travelled (in kilometers) as its only parameter and returns the total fare
-# as its only result rounded by 2 digits. Write a program that demonstrates the function.
-
-def taxi_fare(distance):
-    pass
-
-# examples of usage:
-# taxi_fare(10) #21.86
