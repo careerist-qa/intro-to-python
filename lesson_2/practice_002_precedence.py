94566de1e402256842d37bd32a1760cd7f8c38a6
Renne Rocha <renne@rennerocha.com>
2023-08-18 16:14:46 -0300

    Update content of "Lesson 2: Numbers"
    - Remove old homework and practices
    - Add new homework and practices

---
+++ lesson_2/practice_002_precedence.py
@@ -0,0 +1,10 @@
+# Try the following examples in Terminal and try to figure out
+# why the difference in results
+
+# >>> 4 + 100 / 10 * 5
+
+# >>> 4 + (100 / (10 * 5))
+
+# >>> (4 + 100 / 10) * 5
+
+# >>> ((4 + 100) / 10) * 5
--- lesson_2/tasks/task_2.py
+++
@@ -1,8 +0,0 @@
-# TASK 2
-
-num_cakes = 8
-
-print('Maggie baked', num_cakes, 'cakes in the morning')
-
-# Reassign the num_cakes value
-print('Maggie baked', num_cakes, 'cakes in the afternoon')
