f038b549e76259b10b22dcde941c77ccb9dfffd2
Joseph Sabido <joesabido@gmail.com>
2023-08-25 15:51:43 -0500

    practice files content

---
+++ lesson_7/practice_002_ranges.py
@@ -0,0 +1,17 @@
+# Figure out the values required for range() to generate the expected result:
+
+# 0, 1, 2, 3, 4
+range(???)
+
+# 20, 21, 22 ... 30 (all the numbers between 20 and 30, inclusive)
+range(???)
+
+# Even numbers: 2, 4, 6, 8, 10
+range(???)
+
+# Years ending in zero between 1900 and 2000 (inclusive)
+range(???)
+
+# Bonus!
+# 20, 19, 18, 17, 16
+range(???)
