8705db2f5bdd8f99a10f664dad23c0835546464d
Vitaly Erokhin <vitaly.erokhin@careerist.com>
2023-09-03 15:50:05 +0800

    Add HW to Lesson 5

---
+++ lesson_5/homework.py
@@ -0,0 +1,79 @@
+# Homework Lesson 5 - Workshop - Homework
+
+# READ CAREFULLY THE EXERCISE DESCRIPTION AND SOLVE IT RIGHT AFTER IT
+
+# Challenge 1
+# Make a number Positive
+#
+# Create a variable called my_number and set it to any integer value.
+# Write code to make the number positive if it's negative, and keep it
+# as is if it's already positive or zero.
+#
+# Example:
+# Input: -3 => Output: 3
+# Input: 5 => Output: 5
+
+
+# ---------------------------------------------------------------------
+
+# Challenge 2
+# BinGo!
+#
+# If the number is divisible of 3, print “Bin”
+# If the number is divisible of 7, print “Go”
+# For numbers which are divisible of 3 and 7, print “BinGo”
+# Otherwise, print the original number: “{number} is just a number”
+
+
+# ---------------------------------------------------------------------
+
+# Challenge 3
+# Find the middle number
+#
+# Given three different numbers x, y, and z, find the number that is neither
+# the smallest nor the largest and print it.
+#
+# Example:
+# x = 1, y = 5, z = 3 => Output: 3
+
+
+# ---------------------------------------------------------------------
+
+# Challenge 4
+# Palindrome Numbers
+#
+# Ask a user to input a number.
+# Write a program to check if the given number is a palindrome.
+# It should print True if the number is a palindrome and False if it is not.
+#
+# Palindrome number: 121, 898
+
+
+# ---------------------------------------------------------------------
+
+# Challenge 5
+# Reverse a string
+#
+# You're part of a team working on analyzing customer reviews for a new video game.
+# Due to a software glitch, some reviews have been recorded in reverse with punctuation
+# at the beginning instead of the end. Your task is to correct these reviews so that they
+# are in the correct order and the punctuation is appropriately placed at the end of the
+# sentence or word.
+
+
+# ---------------------------------------------------------------------
+
+# Challenge 6
+# Palindrome Sentence
+# Write a program to check if a given sentence is a palindrome or not.
+# Ignore spaces, punctuation, and capitalization.
+#
+# Hints:
+# You may find the .lower() method useful for ignoring capitalization
+# Remove spaces
+#
+# Example:
+# Input: "A man a plan a canal Panama" => Output: True
+# Input: "Hello World" => Output: False
+
+
